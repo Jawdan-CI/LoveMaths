234cf860d0aec8840bb09d39011ed27770ffca24
Jordan Preedy <9r7icy5m@students.codeinstitute.net>
2024-08-14 11:33:34 +0000

    Added functionality to the Multiply, subtract, and divsion buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+
     runGame("addition");
 });
 
@@ -22,12 +29,21 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame (gameType) {
 
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
+
     // Create two random numbers from 1 - 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}`;
@@ -39,8 +55,10 @@ function checkAnswer () {
     let calculateAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculateAnswer[0];
     if (isCorrect) {
+        incrementScore();
         alert("Correct!");
     } else {
+        incrementWrongAnswer();
         alert (`Incorrect. The Correct answer was ${calculateAnswer[0]}`);
     }
 
@@ -58,18 +76,34 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }  else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
 
-function incrementScore() {
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = oldScore + 1;
+    
 }
 
+/**
+ * Gets the current incorrect score from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = oldScore = 1;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -79,14 +113,21 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
 
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
